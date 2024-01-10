4665029b3d5d60ba7a6c1fb9bcd4c17593419827
MissHarvest <abcdxsky@naver.com>
2024-01-10 17:06:05 +0900

    [🚮Delete] - 바위 에셋 삭제
    수많은 경고 문구 때문에 다른 에셋을 사용하거나 추후에 다시 추가해서 해결할 예정

--- Assets/02. Scripts/Manager/GameManager.cs
+++ Assets/02. Scripts/Manager/GameManager.cs
@@ -5,41 +5,4 @@ public class GameManager
 {
     public Player Player;
 
-    #region Member Variables
-
-    /* Timer Variables */
-    private TimeSpan _gameTime;
-    private TimeSpan _realTime;
-    
-    #endregion
-    
-
-
-    #region Properties
-
-    /* Timers Getter */
-    public TimeSpan GameTime => _gameTime;
-    public TimeSpan RealTime => _realTime;
-    
-    /* InGame State */
-    //public InGameStatement InGameState { get; set; }
-
-    #endregion
-
-
-
-    #region Setter
-
-    /* Timer Setup Events */
-    public void SetupGameTime(TimeSpan gameTime)
-    {
-        _gameTime = gameTime;
-    }
-
-    public void SetupRealTime(TimeSpan realTime)
-    {
-        _realTime = realTime;
-    }
-
-    #endregion
 }
