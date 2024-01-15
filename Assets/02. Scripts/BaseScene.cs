7ed10433e6815f2c2aae85b1d3c9ef634df2a2d6
MissHarvest <abcdxsky@naver.com>
2024-01-15 15:21:15 +0900

    [➕Update] - Add Clock UI
    시간 현황을 볼 수 있는 UI 를 추가했습니다.

--- Assets/02. Scripts/BaseScene.cs
+++ Assets/02. Scripts/BaseScene.cs
@@ -9,16 +9,17 @@ public class BaseScene : MonoBehaviour
     {
         Managers.Resource.LoadAllAsync<UnityEngine.Object>("MainScene", (key, count, total) => 
         {
-            //Debug.Log($"{count}/{total}: {key} load success");
-
             if (count == total)
             {
+                Managers.Game.Init();
+
                 var player = Managers.Resource.GetCache<GameObject>("Player.prefab");
                 player = Instantiate(player);
                 player.name = "Player";
                 virtualCamera.Follow = Managers.Game.Player.ViewPoint;
                 virtualCamera.LookAt = Managers.Game.Player.ViewPoint;
 
+                
                 Managers.UI.ShowSceneUI<UIMainScene>();
                 Managers.UI.LoadPopupUIs();
                 Managers.Data.InitializeRecipeData();
@@ -27,14 +28,5 @@ public class BaseScene : MonoBehaviour
                 Instantiate(mon);
             }
         });
-
-        //var player = Managers.Resource.Instantiate("Player");
-        //player.name = "Player";
-        //virtualCamera.Follow = Managers.Game.Player.ViewPoint;
-        //virtualCamera.LookAt = Managers.Game.Player.ViewPoint;
-
-        //// Managers.UI.ShowScene
-        //Managers.UI.ShowSceneUI<UIMainScene>();
-        //Managers.UI.LoadPopupUIs();
     }
 }
