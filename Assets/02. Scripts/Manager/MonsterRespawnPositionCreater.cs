179840af126a0120813cc2ddebf9d7f6679e3c34
MissHarvest <abcdxsky@naver.com>
2024-01-17 21:34:30 +0900

    [➕Update] - Monster Spawn Manager, Filtering
    # 땅이 없는 부분 스폰 지역에서 제외
    # 가장자리에서 일정 거리를 스폰 지역에서 제외

--- Assets/02. Scripts/Manager/MonsterRespawnPositionCreater.cs
+++ Assets/02. Scripts/Manager/MonsterRespawnPositionCreater.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Transactions;
+using Unity.VisualScripting;
 using UnityEngine;
 
 // 2024. 01. 16 Park Jun Uk
 public class MonsterRespawnPositionCreater : MonoBehaviour
 {
     [Header("Ground Size")]
-    public float width= 300.0f;
-    public float length = 300.0f;
-
+    public Vector3 isolationCenter;
+    public int isolationRadius = 300; // World Size
+    public int boundary = 3;
     public struct Node
     {
         public bool locked;
@@ -22,15 +24,15 @@ public class MonsterRespawnPositionCreater : MonoBehaviour
     public GameObject resource;
     public GameObject area;
     public int range = 10;
-    public int count = 3;
-    public int field = 0;
+    public int count = 3; // 배치할 몬스터 수
+    private int _field = 0;
 
     public Node[,] nodes;
 
     public int monsterInterval = 1;
     private Vector3 _offset;
-    private float _interval = 4.5f;
-    public float boundary = 5.0f;
+    private int _interval;
+    
     [field:SerializeField] private List<GameObject> _monsters = new List<GameObject>();
     private List<GameObject> _resources = new List<GameObject>();
     public int centerRadius = 5;
@@ -41,9 +43,11 @@ public class MonsterRespawnPositionCreater : MonoBehaviour
     private void Awake()
     {
         nodes = new Node[range, range];
-        field = range * range;
+        _field = range * range;
 
-        _offset = new Vector3((transform.localScale.x - boundary) / 2, 0, (transform.localScale.z - boundary) / 2);
+        _interval = 5;// (isolationRadius-1) / range;
+
+        _offset = new Vector3(isolationRadius / 2, 0, isolationRadius/ 2);
     }
 
     private void Start()
@@ -51,32 +55,34 @@ public class MonsterRespawnPositionCreater : MonoBehaviour
         Stopwatch watch = new Stopwatch();
         watch.Start();
 
-        bool finished = false;
-        while(!finished)
-        {
-            ResetData();
+        CheckUnusableNodes();
+        
+        LockBoundary();
 
-            var d = range / 2;
-            
-            for(int x = d - centerRadius; x <= d + centerRadius; ++x)
-            {
-                for(int z = d - centerRadius; z <= d + centerRadius; ++z)
-                {
-                    //Debug.Log($"Area ; [{x},{z}]");
-                    nodes[x, z].locked = true;
-                    var pos = new Vector3(x * _interval, 2, z * _interval) - _offset;
-                    var go = Instantiate(area , pos, Quaternion.identity);
-                    _resources.Add(go);
-                    --field;
-                }
-            }
+        //for (int x = 0; x < nodes.GetLength(0); ++x)
+        //{
+        //    for (int z = 0; z < nodes.GetLength(1); ++z)
+        //    {
+        //        if (nodes[x, z].locked) continue;
+        //        var pos = new Vector3(x * _interval, 2, z * _interval) - _offset;
+        //        var go = Instantiate(monster, pos, Quaternion.identity);
+        //    }
+        //}
 
-            finished = CreateMonster();
-        }
+        LockCenter();
 
-        // Set Monster Rank,
+        LoopCreateMonster();
+
+        SetMonsterRank();
+
+        watch.Stop();
+        UnityEngine.Debug.Log($"{watch.ElapsedMilliseconds} ms");
+    }
+
+    private void SetMonsterRank()
+    {
         List<(int, float)> dist = new List<(int, float)>();
-        for(int i = 0; i < _monsters.Count; ++i)
+        for (int i = 0; i < _monsters.Count; ++i)
         {
             var d = _monsters[i].transform.position - transform.position;
             d.y = 0;
@@ -88,61 +94,175 @@ public class MonsterRespawnPositionCreater : MonoBehaviour
         dist = dist.OrderBy(x => x.Item2).ToList();
 
         // monster List. 순차적
-        for(int i = 0; i < rankCount[0]; ++i)
+        for (int i = 0; i < rankCount[0]; ++i)
         {
             _monsters[dist[i].Item1].GetComponent<Light>().color = colors[2];
         }
 
-        for(int i = rankCount[0]; i < rankCount[0] + rankCount[1]; ++i)
+        for (int i = rankCount[0]; i < rankCount[0] + rankCount[1]; ++i)
         {
             _monsters[dist[i].Item1].GetComponent<Light>().color = colors[1];
         }
-
-        watch.Stop();
-        UnityEngine.Debug.Log($"{watch.ElapsedMilliseconds} ms");
     }
 
-    private void ResetData()
+    private void LoopCreateMonster()
+    {
+        bool finished = false;
+        while (!finished)
+        {
+            ResetData();
+            UnityEngine.Debug.Log($"Create Monster Start[{_field}]");
+            finished = CreateMonster((Node[,])nodes.Clone(), _field);
+            UnityEngine.Debug.Log($"Create Monster End[{_field}] [{finished}]");
+        }
+    }
+
+    private void CheckUnusableNodes()
+    {
+        float maxDistance = 100;
+        for (int x = 0; x < nodes.GetLength(0); ++x)
+        {
+            for(int z = 0; z < nodes.GetLength(1); ++z)
+            {
+                RaycastHit hit;
+                var start = new Vector3(x * _interval, 10, z * _interval) - _offset;
+                if(!Physics.BoxCast(start, Vector3.one * 0.5f, Vector3.down, out hit, Quaternion.identity, maxDistance))
+                {
+                    nodes[x, z].locked = true;
+                    --_field;
+                }
+            }
+        }
+    }
+
+    private void LockBoundary()
+    {
+        int cnt = boundary;
+        Vector2[] directions = { new Vector2(-1, 0), new Vector2(0, 1), new Vector2(1, 0), new Vector2(0, -1) };
+        List<int> directionIndexs = new List<int>();
+
+        while (cnt != 0)
+        {
+            //UnityEngine.Debug.Log($"=========={cnt}===========");
+            if(TryGetBoundaryStartPoint(out Vector2 start)== false) return;
+            
+
+            var current = start;
+            bool stop = false;
+            int forward = 0;
+                        
+            while (!stop)
+            {
+                //UnityEngine.Debug.Log($"Current Position [{current.x}, {current.y}]");
+                nodes[(int)current.x, (int)current.y].locked = true;
+                --_field;
+                GetIndexsToDecideDirection(directionIndexs, forward);
+
+                for (int i = 0; i < directionIndexs.Count; ++i)
+                {
+                    var next = current + directions[directionIndexs[i]];
+                    //UnityEngine.Debug.Log($"Check Position [{next.x}, {next.y}]");
+                    if (next.x < 0 || next.x >= range || next.y < 0 || next.y >= range) continue;
+                                        
+                    if (nodes[(int)(next.x), (int)(next.y)].locked == false)
+                    {
+                        current = next;
+                        forward = directionIndexs[i];
+                        break;
+                    }
+                    else
+                    {
+                        if (start == current + directions[directionIndexs[i]])
+                        {
+                            stop = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            --cnt;
+        }
+    }
+
+    private void LockCenter()
+    {
+        var d = range / 2;
+
+        for (int x = d - centerRadius; x <= d + centerRadius; ++x)
+        {
+            for (int z = d - centerRadius; z <= d + centerRadius; ++z)
+            {
+                //Debug.Log($"Area ; [{x},{z}]");
+                if (nodes[x, z].locked == false)
+                {
+                    nodes[x, z].locked = true;                    
+                    --_field;
+                }
+                
+                var pos = new Vector3(x * _interval, 2, z * _interval) - _offset;
+                Instantiate(area, pos, Quaternion.identity);
+            }
+        }
+    }
+
+    private bool TryGetBoundaryStartPoint(out Vector2 start)
     {
         for (int x = 0; x < nodes.GetLength(0); ++x)
         {
             for (int z = 0; z < nodes.GetLength(1); ++z)
             {
-                nodes[x, z].locked = false;
+                if (nodes[x, z].locked == false)
+                {
+                    start = new Vector2(x, z);
+                    return true;
+                }
             }
         }
+        start = Vector2.zero;
+        return false;
+    }
 
+    private void GetIndexsToDecideDirection(List<int> list, int forward)
+    {
+        list.Clear();
+        for (int i = 0; i < 3; ++i)
+        {
+            list.Add((forward + 3 + i) % 4);
+        }
+    }
+
+    private void ResetData()
+    {
         foreach (var go in _monsters)
         {
             Destroy(go);
         }
         _monsters.Clear();
 
-        foreach (var go in _resources)
+        foreach(var go in _resources)
         {
             Destroy(go);
         }
         _resources.Clear();
-        field = range * range;
     }
 
-    private bool CreateMonster()
+    private bool CreateMonster(Node[,] usableNodes, int field)
     {
         int cnt = count;
-        for (int x = 0; x < nodes.GetLength(0); ++x)
+        for (int x = 0; x < usableNodes.GetLength(0); ++x)
         {
-            for (int z = 0; z < nodes.GetLength(1); ++z)
+            for (int z = 0; z < usableNodes.GetLength(1); ++z)
             {
                 if (0 == cnt) return true;
-                if (nodes[x, z].locked) continue;
+                if (usableNodes[x, z].locked) continue;
 
                 // 배치할 갯수 / 남은 공간 과 랜덤 비교
                 var radio = (float)cnt / field;
-                var ran = UnityEngine.Random.Range(0.0f, 1.0f);
 
                 //Debug.Log($"[{x},{z}] Cube / Field {cnt} / {field} = {radio}");
                 --field;
-                nodes[x, z].locked = true;
+                usableNodes[x, z].locked = true;
                 if (!RepeatPercent(radio)) continue;
 
                 // 배치 하기
@@ -154,7 +274,7 @@ public class MonsterRespawnPositionCreater : MonoBehaviour
                 --cnt;
 
                 // 주변에 _interval 만큼 잠구기. 본인 포함
-                LockAround(x, z);
+                LockAround(ref usableNodes, x, z, ref field);
             }
         }
 
@@ -171,7 +291,7 @@ public class MonsterRespawnPositionCreater : MonoBehaviour
         return false;
     }
 
-    private void LockAround(int x, int z)
+    private void LockAround(ref Node[,] usableNodes, int x, int z, ref int field)
     {
         var minX = Math.Max(0, x - monsterInterval);
         var maxX = Math.Min(range - 1, x + monsterInterval);
@@ -187,8 +307,8 @@ public class MonsterRespawnPositionCreater : MonoBehaviour
                 var abs = Mathf.Abs(i - x) + Mathf.Abs(j - z);
                 if (monsterInterval >= abs)
                 {
-                    if (nodes[i, j].locked == false) --field;
-                    nodes[i, j].locked = true;
+                    if (usableNodes[i, j].locked == false) --field;
+                    usableNodes[i, j].locked = true;
                     //Debug.Log($"LOCK [{i},{j}]");
                     var pos = new Vector3(i * _interval, 1, j * _interval) - _offset;
                     var go = Instantiate(resource, pos, Quaternion.identity);
@@ -196,11 +316,5 @@ public class MonsterRespawnPositionCreater : MonoBehaviour
                 }
             }
         }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawCube(transform.position, new Vector3(width, 1, length));
     }
 }
