b5137519c22806e0fb4545345f436a683b07707b
정민 <jeongmin322@naver.com>
2024-01-17 10:23:59 +0900

    [➕Update] Refactor DataManager
    데이터매니저 리팩토링, 파일 및 폴더 정리

---
+++ Assets/02. Scripts/Data/BonFire.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BonFire : MonoBehaviour
+{
+    public void DropFireFromPlayerHand(ItemSlot itemInUse, Dictionary<string, GameObject> tools)
+    {
+        // 현재 플레이어가 들고 있는 도구의 이름을 가져옴
+        string currentToolName = Managers.Game.Player.ToolSystem.GetToolName(itemInUse);
+
+        if (currentToolName == "Handable_BonFire")
+        {
+            // 플레이어의 손에서 모닥불 비활성화
+            gameObject.SetActive(false);
+
+            GameObject droppedTool = Instantiate(gameObject);
+            droppedTool.transform.position = new Vector3(transform.position.x, 2f, transform.position.z + 1f);
+            droppedTool.SetActive(true);
+        }
+    }
+}
