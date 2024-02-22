d7fff02814d7f23873da5dbda8e5f1e807536eb7
MissHarvest <abcdxsky@naver.com>
2024-02-22 19:09:07 +0900

    [ == File Move == ]

---
+++ Assets/02. Scripts/Boss/TerrorBringer.cs
@@ -0,0 +1,16 @@
+
+// 2024. 02. 22 Park Jun Uk
+public class TerrorBringer : BossMonster
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
